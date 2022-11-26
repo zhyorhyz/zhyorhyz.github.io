3beda26fd6f77eac7b4fd8a94b73800ddad75ece
zhyorhyz <zhyaid2022@163.com>
2022-11-26 19:49:40 +0800

    Site updated: 2022-11-26 19:49:38

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="w(ﾟДﾟ)w 去哪了？",clearTimeout(titleTime)):(document.title="♪(^∇^*)抓住你了！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));

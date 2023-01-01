499417c8a138d3e3076131e40175630f69ef23e5
zhyorhyz <115544695+zhyorhyz@users.noreply.github.com>
2023-01-01 09:36:50 +0000

    2024考研计划 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/ab5ccd86.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/7bac32d9.html","posts/82ae12b6.html","posts/ab5ccd86.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
